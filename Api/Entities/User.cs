99d0dc3c921ce243461108cb8b7d86b0d053b1a4
jpedro-cf <jpedroswc453@gmail.com>
2025-10-21 15:08:46 -0300

    refactor: make vault key not nullable

--- Api/Entities/User.cs
+++ Api/Entities/User.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace EncryptionApp.Api.Entities;
@@ -7,7 +8,7 @@ public class User: IdentityUser<Guid>
     // VaultKey: [iv | encrypted key]
     // It's encrypted by a key that's derived by the user's password on account creation
     // If the user changes it's password, we just encrypt it again with the new key
-    public string? VaultKey { get; set; }
+    public string VaultKey { get; set; }
     public virtual ICollection<Folder> Folders { get; set; }
     public virtual ICollection<File> Files { get; set; }
     public virtual ICollection<StorageUsage> StorageUsages { get; set; }
