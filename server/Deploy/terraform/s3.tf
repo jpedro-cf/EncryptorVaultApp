3c2a5f177c3946c58a544127b0cc65f2a01c07b4
jpedro-cf <jpedroswc453@gmail.com>
2025-10-23 22:06:00 -0300

    refactor: removed deploy config from server folder

--- server/Deploy/terraform/s3.tf
+++
@@ -1,5 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "encryption_app_bucket" {
-    bucket = var.DEV_MODE ? "encryption-app-bucket" : "encryption-app-bucket-${data.aws_caller_identity.current.account_id}"
-}
