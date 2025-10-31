ad4c7d8bda3b368113996eeedcd4c0010bfd5e7f
jpedro-cf <jpedroswc453@gmail.com>
2025-10-31 16:19:55 -0300

    ops: terraform config

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "encryption_app_bucket" {
-    bucket = var.DEV_MODE ? "encryption-app-bucket" : "encryption-app-bucket-${data.aws_caller_identity.current.account_id}"
+resource "aws_s3_bucket" "secure_vault_bucket" {
+    bucket = var.DEV_MODE ? "secure-vault-bucket" : "secure-vault-bucket-${data.aws_caller_identity.current.account_id}"
 }
